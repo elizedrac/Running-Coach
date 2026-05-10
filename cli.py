43f280fc936d0fbe61fa7a97051e5aca382c469f
Elize Dracopoulos <elizedrac@gmail.com>
2026-05-10 18:58:24 -0400

    implement initial CLI input parser

--- cli.py
+++ cli.py
@@ -1,17 +1,18 @@
 # CLI entry point (V1). Thin wrapper that calls services/coach.py::ask().
-from db.client import get_supabase_client
-from services.garmin import garmin_sync
-from services.llm import call_llm
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+USER_ID = os.getenv("USER_ID")
 
 def main():
-    # supabase = get_supabase_client()
-    # test = supabase.from_("users").select("*").execute()
-    # print(test)
+    while True:
+        user_input = input("You: ")
+        if user_input.lower().strip() in ["exit", "quit", "q", "bye",""]:
+            break
 
-    # id = "f02a4ceb-0549-4816-ac88-07001be65c70"
-    # garmin_sync(id, "2026-01-08", "2026-01-09")
-
-    # print(call_llm("You are a helpful running coach.", "What should I do for my next run?"))
+        print("Coach not implemnetd yet")
 
 
 if __name__ == "__main__":
